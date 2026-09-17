33c7145f19db1d7dfba49e4f5f394f3357f20261
Florian <103504799+flotttt@users.noreply.github.com>
2026-09-17 14:31:32 +0200

    sonynotch: Homebrew 7 install steps syntax

--- Casks/sonynotch.rb
+++ Casks/sonynotch.rb
@@ -12,14 +12,13 @@ cask "sonynotch" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
 
   app "SonyNotch.app"
 
   # The app isn't notarized yet: without this, macOS refuses to open it after install.
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-dr", "com.apple.quarantine", "#{appdir}/SonyNotch.app"]
+  postflight_steps do
+    run "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "{{appdir}}/SonyNotch.app"]
   end
 
   uninstall quit: "com.semvis123.SonyHeadphonesClient"
