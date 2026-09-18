06ef81351dc4860b63db4a74a047a426c4820032
Florian <103504799+flotttt@users.noreply.github.com>
2026-09-18 15:23:41 +0200

    overhead 1.5.0

--- Casks/overhead.rb
+++ Casks/overhead.rb
@@ -1,8 +1,8 @@
 cask "overhead" do
   version "1.5.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"  # set by the release workflow
+  sha256 "dde0edcedc3db3d7cab2d69713389364bdc9ec6d7669ba043345c85c2ace78ed"
 
-  url "https://github.com/flotttt/Overhead/releases/download/v#{version}/Overhead.zip"
+  url "https://github.com/flotttt/Overhead/releases/download/v1.5.0/Overhead.zip"
   name "Overhead"
   desc "Control Sony headphones and your music from the menu bar and the notch"
   homepage "https://github.com/flotttt/Overhead"
