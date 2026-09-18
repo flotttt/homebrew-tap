76e60d2362bc86ac4d5fe0e525df7e4785d37eac
Florian <103504799+flotttt@users.noreply.github.com>
2026-09-18 09:25:47 +0000

    sonynotch 1.4.0

--- Casks/sonynotch.rb
+++ Casks/sonynotch.rb
@@ -1,8 +1,8 @@
 cask "sonynotch" do
-  version "1.3.0"
-  sha256 "6cf7ca25b9033714d24af40113bb39322e304eca8fe2ccefa7fbb39ea30dd404"
+  version "1.4.0"
+  sha256 "9cc3e4dce7693217e8a6d2efa0b948bb531dfee150b9be40e5752052b8ab8785"
 
-  url "https://github.com/flotttt/SonyNotch/releases/download/v1.3.0/SonyNotch.zip"
+  url "https://github.com/flotttt/SonyNotch/releases/download/v1.4.0/SonyNotch.zip"
   name "SonyNotch"
   desc "Control Sony headphones from the menu bar and the notch"
   homepage "https://github.com/flotttt/SonyNotch"
